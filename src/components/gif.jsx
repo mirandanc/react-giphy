3e76cd7c187698cb1e85ac23b4765838e2cd55cb
arthur miranda <miranda_chaves@hotmail.com>
2022-07-21 22:59:10 -0300

    new gif

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class Gif extends Component {
   render() {
+    const src = "https://www.myconfinedspace.com/wp-content/uploads/tdomf/219931/o89059.gif"
     return (
       <img src={src} alt="" className="gif"/>
     );
