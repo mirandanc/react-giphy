d91108c1bfed6bad6970dceee57c632225460d1c
arthur miranda <miranda_chaves@hotmail.com>
2022-07-22 23:13:24 -0300

    added more gifs

--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import Gif from './gif';
 
 class GifList extends Component {
   render() {
-    <Gif id
+    return this.props.gifs.map(gif => {
+      return <Gif id={Gif.id} key={gif.id}/>
+    });
   }
 }
 
